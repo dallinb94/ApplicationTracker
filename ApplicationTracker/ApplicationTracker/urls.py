7847d2860d1beb26b18f5b88fa17b2e0afabc1b7
DsrKrasniii <donredd3rd@gmail.com>
2022-11-15 11:56:55 -0700

    Co-authored-by: Connor Chytraus <connorchytraus@users.noreply.github.com> Co-authored-by: JSlaugh <JSlaugh@users.noreply.github.com>

--- ApplicationTracker/ApplicationTracker/urls.py
+++ ApplicationTracker/ApplicationTracker/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apptrackpages.urls'))
+    path('', include('apptrackpages.urls')),
 ]
